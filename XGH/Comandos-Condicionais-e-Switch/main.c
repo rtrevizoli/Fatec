b6a3de67ce30019a2358064a4c308a5e139aa227
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2019-09-04 21:04:03 -0300

    Ex03 - done

--- XGH/Comandos-Condicionais-e-Switch/main.c
+++ XGH/Comandos-Condicionais-e-Switch/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 
 void menu() {
     printf("--Options Menu--\n"
            "A) Valida maior, inteiro;\n"
-           "B) Menor que 0 ou Raiz do Maior;\n");
+           "B) Menor que 0 ou Raiz do Maior;\n"
+           "C) Raiz do positivo ou quadrado do negativo;\n");
 }
 
 void ex01() {
@@ -25,10 +27,22 @@ void ex02() {
     printf("Informe um numero: ");
     scanf("%d", &n);
 
-    if (n < 0) {
+    if(n < 0) {
         printf("Invalid Number!");
     } else {
         printf("O quadrado e: %d", n*n);
+    }
+}
+
+void ex03() {
+    float n;
+    printf("Informe um numero: ");
+    scanf("%f", &n);
+
+    if(n < 0) {
+        printf("O quadrado do negativo e: %.2f", n*n);
+    } else {
+        printf("A raiz do positivo e: %.2f", sqrt(n));
     }
 }
 
@@ -44,6 +58,9 @@ int main() {
         break;
     case 'B':
         ex02();
+        break;
+    case 'C':
+        ex03();
         break;
     default:
         printf("Invalid Option!!");
