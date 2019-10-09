751818555c2c1f7607c5aa89bae4e34d59b31dce
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2019-10-09 20:49:10 -0300

    Prova prática, LPI1 - done w caveats

--- XGH/some-lil-ex/main.c
+++ XGH/some-lil-ex/main.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main() {
 
+    int rg[] = {};
+    int len[] = {};
+    int i = 0, maxi = 0;
+
+    for (;;) {
+
+        printf("Informe um numero de RG: ");
+        scanf("%d", &rg[i]);
+
+        int  cd = 0, valor = rg[i];
+        do {
+            cd += 1;
+            valor /= 10;
+        } while (valor != 0);
+        len[i] = cd;
+
+        i += 1;
+        maxi += 1;
+
+        char c = 'w';
+
+        printf("Deseja inserir mais um rg ?[s/ n] ");
+        getchar();
+        scanf("%c", &c);
+
+        if (c == 'n') {
+            break;
+        }
+    }
+
+    for (int i = 0; i < maxi; i++) {
+        if (len[i] == 9) {
+            if (rg[i]/100000000 == 1) {
+                printf("\n---->%i\n", rg[i]);
+            }
+        }
+    }
+
+    //427483268
+    getchar();
     return 0;
 }
