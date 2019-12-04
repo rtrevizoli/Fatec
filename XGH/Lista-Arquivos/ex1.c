6f84c66c5652d8dd07381cbe4ac0456a3c209186
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2019-12-03 22:38:01 -0300

    Print Steps Added

--- XGH/Lista-Arquivos/ex1.c
+++ XGH/Lista-Arquivos/ex1.c
@@ -23,6 +23,26 @@ int main() {
 
     fclose(f);
 
+    f = fopen("arq.txt", "r");
+
+    if (f == NULL) {
+        printf("Erro na abertura!\n");
+        scanf("kkk\n");
+        exit(1);    
+    }
+
+    char c = fgetc(f);
+
+    printf("\nFile Start\n");
+
+    while(c != EOF) {
+        printf("%c\n", c);
+        c = fgetc(f);
+    }
+
+    printf("Game Over\n");
+
+
     scanf("kkk\n");
 
     return 0;
