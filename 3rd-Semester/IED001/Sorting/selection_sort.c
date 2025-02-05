a671ddcde9777d1587832960b7ebdae5c9d4dae1
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-02-05 16:01:38 -0300

    Selection Sort for ED 02/05/2025
    Change-Id: I59cbb206e9a7c757039e40855f05c91f89da4668

--- 3rd-Semester/IED001/Sorting/selection_sort.c
+++ 3rd-Semester/IED001/Sorting/selection_sort.c
@@ -49,6 +49,69 @@ void add(element **first_element, element **last_element, int value)
     *last_element = new_element;
 }
 
+void swap_elements(element **first_element, element *prev_x, element *x, element *prev_y, element *y)
+{
+    // Skipping in case current and lower is equal;
+    if (x == y) return;
+
+    // Swapping prev next, case prev is null set element as head
+    if (prev_x) prev_x->next = y;
+    else *first_element = y;
+
+    if (prev_y) prev_y->next = x;
+    else *first_element = x;
+
+    // Swapping elements
+    element *temp = x->next;
+    x->next = y->next;
+    y->next = temp;
+}
+
+void sort(element **first_element)
+{
+    if (*first_element == NULL) return;
+
+    element *prev_lower_element = NULL;
+    element *lower_element = NULL;
+
+    for (element *i = *first_element, *prev_i = NULL; i && i->next; prev_i = i, i = i->next)
+    {
+        lower_element = i;
+        prev_lower_element = prev_i;
+
+        for (element *j = i->next, *prev_j = i; j; prev_j = j, j = j->next)
+        {
+            printf("[%d][%d]\n", i->value, j->value);
+
+            if (j->value < lower_element->value)
+            {
+                lower_element = j;
+                prev_lower_element = prev_j;
+            }
+        }
+
+        if (i != lower_element)
+        {
+            printf("\nBefore swap\n");
+            printf("*i -> %d\n", i->value);
+            printf("*lower_element -> %d\n", lower_element->value);
+
+            swap_elements(first_element, prev_i, i, prev_lower_element, lower_element);
+            
+            printf("After swap\n");
+            printf("*i -> %d\n", i->value);
+            printf("*lower_element -> %d\n", lower_element->value);
+
+            i = lower_element;
+
+            printf("After reset i\n");
+            printf("*i -> %d\n", i->value);
+            printf("*lower_element -> %d\n", lower_element->value);
+
+        }
+    }
+}
+
 int main()
 {
     element *first_element = NULL;
@@ -60,5 +123,7 @@ int main()
     add(&first_element, &last_element, 22);
     add(&first_element, &last_element, 11);
 
+    print(first_element);
+    sort(&first_element);
     print(first_element);
 }
