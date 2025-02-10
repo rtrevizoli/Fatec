61586273a6d4e9767baa3ef48260e66f205b765f
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-02-10 15:35:50 -0300

    Vec print adjustment
    Change-Id: If3cac9a53f3fbd9cbfcf5cf250d16b31d85545c2

--- 3rd-Semester/IED001/Sorting/insertion-sort/insertion-sort.c
+++ 3rd-Semester/IED001/Sorting/insertion-sort/insertion-sort.c
@@ -3,27 +3,45 @@
 
 void printVec(int v[], int v_size)
 {
-    for (int i = 0; i < v_size; i++)
-        printf("v[%d] = %d\n", i, v[i]);
+   printf("int v[] = { ");
+
+   for (int i = 0; i < v_size; i++)
+   {
+      printf("%d", v[i]);
+
+      if (i + 1 != v_size)
+         printf(", ");
+   }
+
+   printf(" };\n");
 }
 
 void insertionsort (int n, int v[])
 {
-   for (int j = 1; j < n; ++j) {
+   for (int j = 1; j < n; ++j)
+   {
+      printf("J: v[%d] = %d\n", j, v[j]);
+
       int x = v[j];
       int i;
-      for (i = j-1; i >= 0 && v[i] > x; --i) 
+
+      for (i = j-1; i >= 0 && v[i] > x; --i)
+      {
+         printf("I: v[%d] = %d\n", i, v[i]);
          v[i+1] = v[i];
+      }
       v[i+1] = x;
    }
+
+   printVec(v, n);
 }
 
 int main()
 {
-    int v[] = { 4, 7, 124, 8, 12 };
-    int v_size = sizeof(v)/sizeof(v[0]);
+   int v[] = { 4, 7, 124, 8, 12 };
+   int v_size = sizeof(v)/sizeof(v[0]);
 
-    printVec(v, v_size);
+   insertionsort(v_size, v);
 
-    return 0;
+   return 0;
 }
