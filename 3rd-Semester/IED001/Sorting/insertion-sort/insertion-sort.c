5b9c41fcfac3fac7b6530feb146d1d8ccd97503a
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-02-19 20:32:06 -0300

    Another implementation of Insertion Sort
    Change-Id: I1cd8e40ce0fee72eb93cd7e52dfe2f20ec53699b

--- 3rd-Semester/IED001/Sorting/insertion-sort/insertion-sort.c
+++ 3rd-Semester/IED001/Sorting/insertion-sort/insertion-sort.c
@@ -16,6 +16,28 @@ void printVec(int v[], int v_size)
    printf(" };\n");
 }
 
+void rtx_insertionSort(int v[], int v_size)
+{
+   int aux = 0;
+
+   for (int i = 0; i < v_size; i++)
+   {
+      int selected_element = v[i];
+
+      for (int j = i; j >= 0; j--)
+      {
+         // printf("selected_element < v[j] ? => %d < v[%d] => %d < %d\n", selected_element, j, selected_element, v[j]);
+
+         if (selected_element < v[j])
+         {
+            v[j+1] = v[j];
+            v[j] = selected_element;
+            // printVec(v, v_size);
+         } 
+      }
+   }
+}
+
 void insertionSort (int v[], int v_size)
 {
    for (int j = 1; j < v_size; ++j)
@@ -40,13 +62,16 @@ int main()
 {
    // int v[] = { 1, 3, 7, 15 }; // Best case input
    // int v[] = { 7, 15, 3, 1 }; // Normal case input
-   int v[] = { 15, 7, 3, 1 }; // Worst case input
+   // int v[] = { 15, 7, 3, 1 }; // Worst case input
+
+   int v[] = {9, 78, -2, 435, 10, 11, 88, 56489302, -54893, 10, 9, 234, 74, 74};
 
    int v_size = sizeof(v)/sizeof(v[0]);
 
    printVec(v, v_size);
 
-   insertionSort(v, v_size);
+   // insertionSort(v, v_size);
+   rtx_insertionSort(v, v_size);
 
    printVec(v, v_size);
 
