e6a7b84c95761fe75d7432c98ad0b7f40aaecf4a
Rafael Trevizoli <rtrevizoli@yahoo.com.br>
2025-02-19 19:44:09 -0300

    Organizing the prints
    Change-Id: Ief60aefd159c9bb6883dbb9e0c3b5e2439d59d0a

--- 3rd-Semester/IED001/Sorting/insertion-sort/insertion-sort.c
+++ 3rd-Semester/IED001/Sorting/insertion-sort/insertion-sort.c
@@ -16,32 +16,37 @@ void printVec(int v[], int v_size)
    printf(" };\n");
 }
 
-void insertionsort (int v[], int v_size)
+void insertionSort (int v[], int v_size)
 {
    for (int j = 1; j < v_size; ++j)
    {
-      printf("J: v[%d] = %d\n", j, v[j]);
-
       int x = v[j];
       int i;
+      printf("J: %d\n", j);
 
       for (i = j-1; i >= 0 && v[i] > x; --i)
       {
-         printf("I: v[%d] = %d\n", i, v[i]);
+         printf("I: %d\n", i);
          v[i+1] = v[i];
       }
+
       v[i+1] = x;
+
+      printf("Iteration: %d, j: %d, x: %d, i: %d, v[i]: %d, v[i+1]: %d\n", j, j, x, i, v[i], v[i+1]);
    }
 }
 
 int main()
 {
-   int v[] = { 4, 7, 124, 8, 12 };
+   // int v[] = { 1, 3, 7, 15 }; // Best case input
+   // int v[] = { 7, 15, 3, 1 }; // Normal case input
+   int v[] = { 15, 7, 3, 1 }; // Worst case input
+
    int v_size = sizeof(v)/sizeof(v[0]);
 
    printVec(v, v_size);
 
-   insertionsort(v, v_size);
+   insertionSort(v, v_size);
 
    printVec(v, v_size);
 
